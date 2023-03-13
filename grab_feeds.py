454d09b1706145cbbc3dca0ea49d0f53d3fa5f54
phanes <phanes@forge.silogroup.lan>
2023-03-12 22:19:24 -0400

    updated padding and added basic support for some types of github repo feeds

--- grab_feeds.py
+++ grab_feeds.py
@@ -39,21 +39,35 @@ class Article:
     def __init__(self, parent_title, entry, config ):
         self.config = config
         self.parent_blog = parent_title
-        self.title = entry['title']
+        self.url = entry['link']
+
+        if self.url.startswith("https://github.com"):
+            self.is_commit_feed = True
+        else:
+            self.is_commit_feed = False
+
+        if self.is_commit_feed:
+            self.title = re.split('\/', self.url)[-1]
+        else:
+            self.title = entry['title']
         self.author = entry['author']
         self.summary = entry['summary']
         self.content = entry['content'][0]['value']
-        self.url = entry['link']
 
-        self._date_raw = entry['published_parsed']
+        try:
+            self._date_raw = entry['published_parsed']
+        except KeyError:
+            self._date_raw = entry['updated_parsed']
         self._datetime_obj = datetime.fromtimestamp(mktime(self._date_raw))
         self.date = self._datetime_obj.strftime('%Y-%m-%d')
 
         self.tags = list()
-        self._tags_raw = entry['tags']
-        for raw_tag in self._tags_raw:
-            self.tags.append(raw_tag['term'])
-
+        if not self.is_commit_feed:
+            self._tags_raw = entry['tags']
+            for raw_tag in self._tags_raw:
+                self.tags.append(raw_tag['term'])
+        else:
+            self.tags = None
 
 # WPSiteFeed is a python object representing the feed results for one site
 class WPSiteFeed:
@@ -73,8 +87,11 @@ class WPSiteFeed:
         self.entries = list()
         for entry in self._entries_raw:
             article = Article( self.title, entry, self.config )
-            if any( x in self.listen_tags for x in article.tags ):
+            if article.is_commit_feed:
                 self.entries.append( article )
+            else:
+                if any( x in self.listen_tags for x in article.tags ):
+                    self.entries.append( article )
 
 
 class SiteGenerator:
